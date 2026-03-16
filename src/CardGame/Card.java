011f0bc86871ea9f43f10f4a9448e5a6245cf434
bradleecrockett <19703290+bradleecrockett@users.noreply.github.com>
2026-03-16 14:57:29 -0700

    Java Docs for Card

--- src/CardGame/Card.java
+++ src/CardGame/Card.java
@@ -15,21 +15,34 @@ public class Card
     private String face;
     private String suit;
 
+
+    /**
+     * Constructor: Sets the face and suit of the card.
+     */
     public Card (String cardFace, String cardSuit)
     {
         face = cardFace;
         suit = cardSuit;
     }
 
+    /**
+     * Returns a String representation of this Card.
+     */
     public String toString()
     {
         return face + " of " + suit;    
     }
 
+    /**
+     * Returns the face of this Card.
+     */
     public String getFace() {
         return face;
     }
     
+    /**
+     * Returns the suit of this Card.
+     */
     public String getSuit() {
         return suit;
     }
